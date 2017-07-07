728fe55973810cb53fe807b6994c981276b47db2
liurchen <liurchen@hotmail.com>
2017-07-07 14:29:23 +0800

    .

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,8 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
-var concat = require('gulp-concat');
-var cssmin = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var browserify = require('gulp-browserify');
 gulp.task('default',function () {
-    return gulp.src('index.less')
+         gulp.src('index.less')
         .pipe(less())
-        .pipe(concat('index.css'))
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'Safari >0', 'Explorer >0', 'Edge >0', 'Opera >0', 'Firefox >=20'],
-            cascade: true,
-            remove: true
-        }))
-        .pipe(browserify({insertGlobals : true,
-            debug : !gulp.env.production}))
         .pipe(gulp.dest('dist/'));
 });
--- index.js
+++ index.js
@@ -3,5 +3,20 @@
  */
 (function(){
   "use strict";
-
 })();
+
+(function(d, w) {
+  var docEl = d.documentElement,
+      resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+      recalc = function() {
+        var clientWidth = docEl.clientWidth;
+        if (!clientWidth) {
+          return;
+        }
+        docEl.style.fontSize = 100 * (clientWidth / 375) + 'px';
+      };
+
+  if (!d.addEventListener) return;
+  w.addEventListener(resizeEvt, recalc, false);
+  d.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
