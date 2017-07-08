3f7edfcef6b00d202b782e46f0063fb05ad97826
liurchen <liurchen@hotmail.com>
2017-07-08 10:24:03 +0800

    gdsh

--- index.js
+++ index.js
@@ -21,3 +21,21 @@
   d.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
 
+(function() {
+  var mask = document.getElementsByClassName('mask')[0];
+  var selfContent = document.getElementsByClassName('self-content')[0];
+  var btnRule = document.getElementsByClassName('btn-rule')[0];
+  var introClose = document.getElementsByClassName('intro-close')[0];
+  btnRule.addEventListener('click', function () {
+    mask.style.display = 'block';
+    selfContent.style.display = 'block';
+  });
+  mask.addEventListener('click', function () {
+    mask.style.display = 'none';
+    selfContent.style.display = 'none';
+  });
+  introClose.addEventListener('click', function () {
+    mask.style.display = 'none';
+    selfContent.style.display = 'none';
+  });
+})();
