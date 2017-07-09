cd5bf7b6619fdadfb836f9a9543db3117ba5d026
mamayan <mosic.ma@hotmail.com>
2017-07-09 23:44:11 +0800

    scroll

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /**
  * Created by yanma on 17-7-5.
  */
-(function(){
-  "use strict";
-})();
 
 (function(d, w) {
   var docEl = d.documentElement,
@@ -14,7 +11,6 @@
           return;
         }
         docEl.style.fontSize = 100 * (clientWidth / 375) + 'px';
-        console.log(docEl.schemaTypeInfo);
       };
 
   if (!d.addEventListener) return;
@@ -44,3 +40,29 @@
     noScroll.classList.remove('overflow');
   });
 })();
+
+
+function changePic(){
+  var btnImg1=document.getElementsByClassName('lotto')[0];
+  var btnImg2=document.getElementsByClassName('holiday')[0];
+  btnImg1.addEventListener('click',function () {
+    btnImg1.setAttribute('src','images/lotto-focus.png');
+  });
+  btnImg2.addEventListener('click',function () {
+    btnImg2.setAttribute('src','images/holiday-focus.png');
+  });
+}
+changePic();
+
+
+window.onscroll = function () {
+  myFunction();
+};
+function myFunction() {
+  var btnTop = document.getElementById('bar');
+  if(document.body.scrollTop > 1000){
+    btnTop.classList.add('top-bar');
+  }else {
+    btnTop.classList.remove('top-bar');
+  }
+}
